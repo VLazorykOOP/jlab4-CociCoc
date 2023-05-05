43981081890c3cfce1f5ba39072872f054ef4f19
Shkliar Maksym <shkliar.maksym@chnu.edu.ua>
2023-05-05 20:56:32 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -3,69 +3,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Введіть ім'я файлу зі списком слів: ");
-            String wordListFileName = getExistingFileName(scanner);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть шлях до тексового файлу: ");
+        String filePath = scanner.nextLine();
 
-            System.out.print("Введіть ім'я файлу з текстом: ");
-            String textFileName = getExistingFileName(scanner);
+        // Перевірка наявності файлу
+        File file = new File(filePath);
+        while (!file.exists()) {
+            System.out.println("Файл не знайдено. Спробуйте ще раз.");
+            System.out.print("Введіть шлях до тексового файлу: ");
+            filePath = scanner.nextLine();
+            file = new File(filePath);
+        }
 
-            // Зчитування списку слів з файлу
-            Set<String> words = new HashSet<>();
-            BufferedReader wordListReader = new BufferedReader(new FileReader(wordListFileName));
+        // Підрахунок кількості слів
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while ((line = wordListReader.readLine()) != null) {
-                words.add(line.trim());
+            int wordCount = 0;
+            while ((line = reader.readLine()) != null) {
+                wordCount += line.trim().split("\\s+").length;
             }
-            wordListReader.close();
-
-            // Пошук слів у тексті та підрахунок входжень
-            Map<String, Integer> wordCount = new HashMap<>();
-            BufferedReader textReader = new BufferedReader(new FileReader(textFileName));
-            while ((line = textReader.readLine()) != null) {
-                String[] tokens = line.split("\\s+");
-                for (String token : tokens) {
-                    if (words.contains(token)) {
-                        int count = wordCount.containsKey(token) ? wordCount.get(token) : 0;
-                        wordCount.put(token, count + 1);
-                    }
-                }
-            }
-            textReader.close();
-
-            // Сортування слів за кількістю входжень та вивід результатів
-            List<Map.Entry<String, Integer>> wordCountList = new ArrayList<>(wordCount.entrySet());
-            wordCountList.sort(Map.Entry.<String, Integer>comparingByValue().reversed());
-            System.out.println("Слова, за якими здійснювався пошук:");
-            for (Map.Entry<String, Integer> entry : wordCountList) {
-                System.out.printf("%s: %d%n", entry.getKey(), entry.getValue());
-            }
-
-            // Запис результатів у файл
-            System.out.print("Введіть ім'я файлу для збереження результатів: ");
-            String resultFileName = scanner.nextLine();
-            PrintWriter resultWriter = new PrintWriter(new FileWriter(resultFileName));
-            for (Map.Entry<String, Integer> entry : wordCountList) {
-                resultWriter.printf("%s: %d%n", entry.getKey(), entry.getValue());
-            }
-            resultWriter.close();
+            System.out.println("Кількість слів у файлі " + filePath + " : " + wordCount);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static String getExistingFileName(Scanner scanner) {
-        String fileName = "";
-        while (true) {
-            fileName = scanner.nextLine();
-            File file = new File(fileName);
-            if (file.exists()) {
-                break;
-            } else {
-                System.out.println("Файл не знайдено. Введіть інше ім'я файлу:");
-            }
-        }
-        return fileName;
     }
 }
